64ab86c42bbe09ebbbf10d41fa1f665d9dfdda9c
rkzhang <zhangrongk@126.com>
2015-01-21 23:19:50 +0800

    ASAA

--- web-learn/src/main/java/com/weblearn/thread/chapter3/PrintQueue.java
+++ web-learn/src/main/java/com/weblearn/thread/chapter3/PrintQueue.java
@@ -13,7 +13,7 @@ public class PrintQueue {
 	public void printJob(Object document) {
 		try {
 			semaphore.acquire();
-			long duration = (long)(Math.random()*10);
+			long duration = (long)(Math.random()*1000);
 			System.out.printf("%s: PrintQueue: Printing a Job during %d seconds\n", Thread.currentThread().getName(), duration);
 			Thread.sleep(duration);
 		} catch(InterruptedException e) {
@@ -21,6 +21,35 @@ public class PrintQueue {
 		} finally {
 			semaphore.release();
 		}
+	}
+	
+	
+	public class Job implements Runnable {
+
+		private PrintQueue printQueue;
 		
+		public Job(PrintQueue printQueue) {
+			this.printQueue = printQueue;
+		}
+		
+		@Override
+		public void run() {
+			System.out.printf("%s: Going to print a job\n", Thread.currentThread().getName());
+			printQueue.printJob(new Object());
+			System.out.printf("%s: The document has been printed\n", Thread.currentThread().getName());
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		PrintQueue printQueue = new PrintQueue();
+		Thread thread[] = new Thread[10];
+		for (int i = 0; i < 10; i++) {
+			thread[i] = new Thread(printQueue.new Job(printQueue), "Thread" + i);
+		}
+		
+		for (int i = 0; i < 10; i++) {
+			thread[i].start();
+		}
 	}
 }
